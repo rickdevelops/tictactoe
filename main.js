f3fb22f81273ac4271fa334e233fd31a493a9f32
Sounak Dey <31971312+rickdevelops@users.noreply.github.com>
2021-06-28 13:59:05 +0530

    added animation and created reset function

--- main.js
+++ main.js
@@ -37,7 +37,8 @@ $(document).ready(function () {
         $("#five").hasClass("o") &&
         $("#seven").hasClass("o"))
     ) {
-      alert("O has won the game. Start a new game");
+    $(".js-container").removeClass("congrats-container");
+    alert("O has won the game. Start a new game");
       $("#game li").text("+");
       $("#game li").removeClass("disable");
       $("#game li").removeClass("o");
@@ -70,7 +71,8 @@ $(document).ready(function () {
         $("#five").hasClass("x") &&
         $("#seven").hasClass("x"))
     ) {
-      alert("X wins has won the game. Start a new game");
+    $(".js-container").removeClass("congrats-container");
+    alert("X wins has won the game. Start a new game");
       $("#game li").text("+");
       $("#game li").removeClass("disable");
       $("#game li").removeClass("o");
@@ -118,7 +120,8 @@ $(document).ready(function () {
           $("#five").hasClass("o") &&
           $("#seven").hasClass("o"))
       ) {
-        alert("O wins");
+    $(".js-container").addClass("congrats-container");
+    alert("O wins");
         count = 0;
         o_win++;
         $("#o_win").text(o_win);
@@ -153,7 +156,8 @@ $(document).ready(function () {
           $("#five").hasClass("x") &&
           $("#seven").hasClass("x"))
       ) {
-        alert("X wins");
+    $(".js-container").addClass("congrats-container");
+    alert("X wins");
         count = 0;
         x_win++;
         $("#x_win").text(x_win);
@@ -162,6 +166,7 @@ $(document).ready(function () {
   });
   $("#reset").click(function () {
     $("#game li").text("+");
+    $(".js-container").removeClass("congrats-container");
     $("#game li").removeClass("disable");
     $("#game li").removeClass("o");
     $("#game li").removeClass("x");
@@ -169,4 +174,68 @@ $(document).ready(function () {
     $("#game li").removeClass("btn-info");
     count = 0;
   });
+  // function resetBoard(){
+  //   $(".js-container").removeClass("congrats-container");
+  //   $("#game li").text("+");
+  //   $("#game li").removeClass("disable");
+  //   $("#game li").removeClass("o");
+  //   $("#game li").removeClass("x");
+  //   $("#game li").removeClass("btn-primary");
+  //   $("#game li").removeClass("btn-info");
+  //   count = 0;
+  // }
 });
+
+// congrats animation
+const Confettiful = function(el) {
+  this.el = el;
+  this.containerEl = null;
+  
+  this.confettiFrequency = 3;
+  this.confettiColors = ['#EF2964', '#00C09D', '#2D87B0', '#48485E','#EFFF1D'];
+  this.confettiAnimations = ['slow', 'medium', 'fast'];
+  
+  this._setupElements();
+  this._renderConfetti();
+};
+
+Confettiful.prototype._setupElements = function() {
+  const containerEl = document.createElement('div');
+  const elPosition = this.el.style.position;
+  
+  if (elPosition !== 'relative' || elPosition !== 'absolute') {
+    this.el.style.position = 'relative';
+  }
+  
+  containerEl.classList.add('confetti-container');
+  
+  this.el.appendChild(containerEl);
+  
+  this.containerEl = containerEl;
+};
+
+Confettiful.prototype._renderConfetti = function() {
+  this.confettiInterval = setInterval(() => {
+    const confettiEl = document.createElement('div');
+    const confettiSize = (Math.floor(Math.random() * 3) + 7) + 'px';
+    const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
+    const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
+    const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
+    
+    confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+    confettiEl.style.left = confettiLeft;
+    confettiEl.style.width = confettiSize;
+    confettiEl.style.height = confettiSize;
+    confettiEl.style.backgroundColor = confettiBackground;
+    
+    confettiEl.removeTimeout = setTimeout(function() {
+      confettiEl.parentNode.removeChild(confettiEl);
+    }, 3000);
+    
+    this.containerEl.appendChild(confettiEl);
+  }, 25);
+};
+
+window.confettiful = new Confettiful(document.querySelector('.js-container'));
+
+
