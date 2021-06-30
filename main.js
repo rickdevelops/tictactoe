a587b3a0050202fa0690d3ed3880a4b335257dea
Sounak Dey <31971312+rickdevelops@users.noreply.github.com>
2021-06-30 14:11:33 +0530

    change Winning number

--- main.js
+++ main.js
@@ -15,6 +15,13 @@ $(document).ready(function () {
   $("#x_win").text(x_win);
   $("#game li").click(function () {
     // console.log(count);
+    if ($("#playWithPCCheck").is(':checked')&& count<8) {
+      // console.log("playing");
+      $(this).text(o);
+      $(this).addClass("disable o btn-info");
+      playWithPC(count);
+      count++;
+    }
     if (count % 2 == 0) {
       $("#currentMarker").html("X");
     } else {
@@ -48,6 +55,9 @@ $(document).ready(function () {
         $("#seven").hasClass("o"))
     ) {
       $(".js-container").removeClass("congrats-container");
+      if ($("#playWithPCCheck").is(':checked')){
+        o_win++;
+      }
       localStorage.setItem("o_win", o_win);
       // alert("O has won the game. Start a new game");
       modal.style.display = "block";
@@ -86,6 +96,9 @@ $(document).ready(function () {
         $("#five").hasClass("x") &&
         $("#seven").hasClass("x"))
     ) {
+      if ($("#playWithPCCheck").is(':checked')){
+        x_win++;
+      }
       localStorage.setItem("x_win", x_win);
       $(".js-container").removeClass("congrats-container");
       // alert("X wins has won the game. Start a new game");
@@ -210,9 +223,46 @@ $(document).ready(function () {
       }
     }
   });
+  $("#playWithPCCheck").click(function () {
+    checkboxPlayWithPC();
+  });
   $("#reset").click(function () {
     resetBoard();
   });
+  $("#reset-everything").click(function () { resetEverything(); });
+
+  // Play with PC
+  function checkboxPlayWithPC() {
+    resetEverything();
+  }
+  function playWithPC(count) {
+    var allIds = ["#one", "#two", "#three", "#four", "#five", "#six", "#seven", "#eight", "#nine"];
+    var randomNumber = getRandomInt(0, 8);
+    // console.log("count",count);
+    // // console.log(document.querySelector("#playWithPCCheck").checked);
+    // if (document.querySelector("#playWithPCCheck").checked) {
+    //   while(($(allIds[randomNumber]).hasClass("o")||$(allIds[randomNumber]).hasClass("x")) && count==8){
+    //   randomNumber= getRandomInt(0,10);
+    //   }
+    //   count++;
+    // }
+    if (document.querySelector("#playWithPCCheck").checked && count < 8) {
+      // console.log(getRandomInt(0, 10));
+      while (($(allIds[randomNumber]).hasClass("disable o btn-info") || $(allIds[randomNumber]).hasClass("disable x btn-info"))) {
+        randomNumber = getRandomInt(0, 8);
+      }
+      console.log(randomNumber);
+      $(allIds[randomNumber]).text(x);
+      $(allIds[randomNumber]).addClass("disable x btn-info");
+    }
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   function resetBoard() {
     $(".js-container").removeClass("congrats-container");
     $("#game li").text("+");
@@ -223,7 +273,23 @@ $(document).ready(function () {
     $("#game li").removeClass("btn-info");
     count = 0;
   }
-  $("#reset-everything").click(function () {
+
+  function resetEverything() {
+    resetBoard();
+    // console.log(x_win,o_win);
+    modal.style.display = "block";
+    if (x_win > o_win) {
+      $(".modal-text").text("W wins.");
+    }
+    else if (x_win < o_win) {
+      $(".modal-text").text("O wins.");
+    }
+    else if (x_win > 0 && o_win > 0 && x_win == o_win) {
+      $(".modal-text").text("It's a Tie.");
+    }
+    else {
+      modal.style.display = "none";
+    }
     localStorage.removeItem("o_win");
     localStorage.removeItem("x_win");
     $("#game li").text("+");
@@ -238,93 +304,93 @@ $(document).ready(function () {
     x_win = 0;
     $("#o_win").text(o_win);
     $("#x_win").text(x_win);
-  });
-});
+  }
 
-// modal
-// Get the modal
-var modal = document.getElementById("myModal");
+  // modal
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+  // Get the button that opens the modal
+  // var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
+  // When the user clicks the button, open the modal
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
     modal.style.display = "none";
-  }
-};
+  };
 
-// congrats animation
-const Confettiful = function (el) {
-  this.el = el;
-  this.containerEl = null;
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
-  this.confettiFrequency = 3;
-  this.confettiColors = ["#EF2964", "#00C09D", "#2D87B0", "#48485E", "#EFFF1D"];
-  this.confettiAnimations = ["slow", "medium", "fast"];
+  // congrats animation
+  const Confettiful = function (el) {
+    this.el = el;
+    this.containerEl = null;
 
-  this._setupElements();
-  this._renderConfetti();
-};
+    this.confettiFrequency = 3;
+    this.confettiColors = ["#EF2964", "#00C09D", "#2D87B0", "#48485E", "#EFFF1D"];
+    this.confettiAnimations = ["slow", "medium", "fast"];
 
-Confettiful.prototype._setupElements = function () {
-  const containerEl = document.createElement("div");
-  const elPosition = this.el.style.position;
+    this._setupElements();
+    this._renderConfetti();
+  };
 
-  if (elPosition !== "relative" || elPosition !== "absolute") {
-    this.el.style.position = "relative";
-  }
+  Confettiful.prototype._setupElements = function () {
+    const containerEl = document.createElement("div");
+    const elPosition = this.el.style.position;
 
-  containerEl.classList.add("confetti-container");
+    if (elPosition !== "relative" || elPosition !== "absolute") {
+      this.el.style.position = "relative";
+    }
 
-  this.el.appendChild(containerEl);
+    containerEl.classList.add("confetti-container");
 
-  this.containerEl = containerEl;
-};
+    this.el.appendChild(containerEl);
 
-Confettiful.prototype._renderConfetti = function () {
-  this.confettiInterval = setInterval(() => {
-    const confettiEl = document.createElement("div");
-    const confettiSize = Math.floor(Math.random() * 3) + 7 + "px";
-    const confettiBackground =
-      this.confettiColors[
+    this.containerEl = containerEl;
+  };
+
+  Confettiful.prototype._renderConfetti = function () {
+    this.confettiInterval = setInterval(() => {
+      const confettiEl = document.createElement("div");
+      const confettiSize = Math.floor(Math.random() * 3) + 7 + "px";
+      const confettiBackground =
+        this.confettiColors[
         Math.floor(Math.random() * this.confettiColors.length)
-      ];
-    const confettiLeft = Math.floor(Math.random() * this.el.offsetWidth) + "px";
-    const confettiAnimation =
-      this.confettiAnimations[
+        ];
+      const confettiLeft = Math.floor(Math.random() * this.el.offsetWidth) + "px";
+      const confettiAnimation =
+        this.confettiAnimations[
         Math.floor(Math.random() * this.confettiAnimations.length)
-      ];
+        ];
 
-    confettiEl.classList.add(
-      "confetti",
-      "confetti--animation-" + confettiAnimation
-    );
-    confettiEl.style.left = confettiLeft;
-    confettiEl.style.width = confettiSize;
-    confettiEl.style.height = confettiSize;
-    confettiEl.style.backgroundColor = confettiBackground;
+      confettiEl.classList.add(
+        "confetti",
+        "confetti--animation-" + confettiAnimation
+      );
+      confettiEl.style.left = confettiLeft;
+      confettiEl.style.width = confettiSize;
+      confettiEl.style.height = confettiSize;
+      confettiEl.style.backgroundColor = confettiBackground;
 
-    confettiEl.removeTimeout = setTimeout(function () {
-      confettiEl.parentNode.removeChild(confettiEl);
-    }, 3000);
+      confettiEl.removeTimeout = setTimeout(function () {
+        confettiEl.parentNode.removeChild(confettiEl);
+      }, 3000);
 
-    this.containerEl.appendChild(confettiEl);
-  }, 25);
-};
+      this.containerEl.appendChild(confettiEl);
+    }, 25);
+  };
 
-window.confettiful = new Confettiful(document.querySelector(".js-container"));
+  window.confettiful = new Confettiful(document.querySelector(".js-container"));
+});
